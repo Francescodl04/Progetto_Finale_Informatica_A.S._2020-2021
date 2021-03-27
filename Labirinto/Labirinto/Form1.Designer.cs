7376998d45becce19d2a2a0c5fa012ce8ea060d5
Francescodl04 <72445602+Francescodl04@users.noreply.github.com>
2021-03-27 20:44:52 +0100

    Modifiche

--- Labirinto/Labirinto/Form1.Designer.cs
+++ Labirinto/Labirinto/Form1.Designer.cs
@@ -28,20 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 47);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Crediti";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(300, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Benvenuto nel gioco del Labirinto! Premi uno dei seguenti pulsati per iniziare!";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(304, 117);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gioca!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(491, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(295, 117);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Come si fa a giocare?";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(172, 392);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(614, 49);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Esci dal gioco";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Labirinto.Properties.Resources.logo1;
+            this.pictureBox1.Location = new System.Drawing.Point(247, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(477, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 442);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::Labirinto.Properties.Resources.fondo_pag_iniziale1;
+            this.ClientSize = new System.Drawing.Size(940, 493);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Labirinto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
